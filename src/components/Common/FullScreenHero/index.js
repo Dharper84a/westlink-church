71b4b24d7ecf6e2ef6859346c1d2beb7da4ec374
DonaldMKS <donald@ascentialmarketing.com>
2023-02-15 19:13:16 -0600

    did things

---
+++ src/components/Common/FullScreenHero/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Image from 'next/image';
+
+import { FullScreenHeroBox } from './styles';
+const FullScreenHero = (props) => {
+
+    return(
+        <FullScreenHeroBox>
+            FullScreenHero
+        </FullScreenHeroBox>
+
+    )
+}
+
+export default FullScreenHero
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Image from 'next/image';
+import FullScreenHero from '../components/Common/FullScreenHero';
+
+
+const TestPage = (props) => {
+
+    return(
+        <FullScreenHero />
+    )
+}
+
+export default TestPage
