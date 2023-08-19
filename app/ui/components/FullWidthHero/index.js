cb037c0af0d7631992c8e46102db0ce1abe115eb
DonaldMKS <donald@ascentialmarketing.com>
2023-08-19 17:45:47 -0500

    hero update

--- app/ui/components/FullWidthHero/index.js
+++ app/ui/components/FullWidthHero/index.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
+
 import { _FullWidthHero, _Inner} from "./styles";
 
 const FullWidthHero = (props) => {
+    const [imageOffset, setImageOffset] = useState(0);
+    const parallaxHandler = () => {
+        const x = (window.scrollY + 1) / 2;
+        setImageOffset(x);
+    }
 
+    useEffect(() => {
+        window.addEventListener("scroll", parallaxHandler)
+        parallaxHandler();
+
+        return () => {
+            window.removeEventListener("scroll", parallaxHandler);
+        }
+    }, [])
     return (
-        <_FullWidthHero>
+        <_FullWidthHero imageOffset={imageOffset}>
+            <Image 
+                src="/images/pink-purple-mirage-4k.jpg"
+                width={1920}
+                height={1080}
+                alt=""
+            />
             <_Inner>
                 <h1>Westlink Church of Christ</h1>
                 <p>
--- app/ui/layouts/Default/Header/styles.js
+++ app/ui/layouts/Default/Header/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const _Header = styled.header`
     position: fixed;
+    z-index: ${({theme}) => theme.layer.content + 10};
     width: 100%;
     padding: 2rem 0;
     background: ${({theme}) => theme.color.white};
